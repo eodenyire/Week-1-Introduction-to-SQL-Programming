07261087340e1cc26af500917d6b8dda0b402dd7
Emmanuel Odenyire Anyira <61192633+odenyirechristopher@users.noreply.github.com>
2022-10-24 23:25:31 +0300

    Add files via upload
    My first commit

---
+++ Week 1 Assignments Introduction to SQL Programming.sql
@@ -0,0 +1,139 @@
+Safaricom Digital Academy Data Egnineering Cohort 4 assignment
+
+*//* 1. Which branch had the highest gross income?
+--------------------------------------------------------
+We use the sum() function and "as" to 
+rename specified column. We also wrap the column 
+"gross income" with quotes since it has spaces 
+in between
+
+Output:
+
+# A - 5057.160500000002
+# B - 5057.032000000003
+# C - 5265.176500000002
+*/
+
+/* Observation:-
+
+Branch C had the highest gross income of $ 5265.18 followed by Branch A and B 
+with gross income of $ 5057.16 and $ 5057.03 
+respectively.
+
+*/
+
+*//* 2. Which branch was the highest-rated?
+-------------------------------------------------------------
+
+Output:
+#A	7.027058823529413
+#B	6.8180722891566266
+#C	7.072865853658538
+
+*/
+
+/* Observation:-
+
+Branch C was the highest rated 
+with a rating of 7.07, while Branch A and B 
+had a rating of 7.03 and 6.82 respectively.
+
+*/
+
+*//* 3. Which branch was the lowest-rated? 
+-------------------------------------------------------------------
+
+Output:
+#A	7.027058823529413
+#B	6.8180722891566266
+#C	7.072865853658538
+
+Observation:-
+
+/*
+Branch B was the lowest rated with a rating 6.8180722891566266
+#B	6.8180722891566266
+
+*/
+
+/* 4. Should the company spend more costs on 
+advertising to normal clients or clients 
+who are members? - 
+-------------------------------------------------------------------------
+
+Output:
+
+# Member - 	7820.164000000002
+# Customers - 501
+#
+#Normal	7559.205000000003
+#499 customers
+
+Observation:-
+
+Spend more on advertising to ormal Customers
+
+*/
+
+/* 5. What type of products should the company 
+focus on increasing marketing sales? 
+--------------------------------------------------------------
+
+Output from the data:
+
+#Health and beauty - 854
+#Electronic accessories - 971
+#Home and lifestyle - 911
+#Sports and travel - 920
+#Food and beverages - 952
+#Fashion accessories - 902
+
+Observation:-
+
+The products to focus on should be Electronic accessories - 971
+
+
+*/
+
+/* 6. What type of products should the company focus on reducing marketing costs? 
+We decide to investigate the types of products 
+which have the lowest gross income.
+----------------------------------------------------
+
+Output from the data:
+#Health and beauty - 854
+#Electronic accessories - 971
+#Home and lifestyle - 911
+#Sports and travel - 920
+#Food and beverages - 952
+#Fashion accessories - 902
+*/
+
+/*  
+
+Observation:-
+
+Focus on Health and beauty products - 854
+  
+/*
+
+/* 7.Should the company invest in their own payments systems if they are outsourcing all
+payment methods?
+--------------------------------------------------------------------------------------------
+Output from the data:-
+
+# AVG(gross_margin_percentage)
+# 4.761904762000012 - Credit card
+# 4.761904762000019 - cash
+# 4.761904762000019 - Ewallet
+
+# Ewallet - cust 345 - Avg Inc- 318.8206 - cogs 303.63866666666667
+# Cash - 344 cust , Avg Inc - 326.18188953488357   cogs - 310.6494186046513
+#Credit card-  Cust 311 - avg inc- 324.009877813505, cogs - 308.58083601286165
+
+
+Observation:-
+
+The company should not invest but should continue outsourcing since the average in terms of margins is basically the same as illustarted below:
+
+
